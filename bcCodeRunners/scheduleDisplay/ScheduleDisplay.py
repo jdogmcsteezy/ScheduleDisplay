02f81b934306ce02126fa468a3ae3663e6f03692
jdogmcsteezy <jesse.sasquatch.schmidt@gmail.com>
2018-01-20 15:47:32 -0800

    complete loadclasses function and test

--- bcCodeRunners/scheduleDisplay/ScheduleDisplay.py
+++ bcCodeRunners/scheduleDisplay/ScheduleDisplay.py
@@ -1,14 +1,20 @@
 from pygame import Surface, time, font
+from BCScheduleCreator import ConvertMilitaryToStd, DoesClassMeet, PrintClass
 import os
+import datetime
+import sys
+import json
 
 class ScheduleDisplay(Surface):
-	def __init__(self, width, height):
+    def __init__(self, width, height):
         Surface.__init__(self, (width, height))
         font.init()
         self.dir_path = os.path.dirname(os.path.realpath(__file__))
+        # Change this when done testing ----
+        self.scheduleFile = 'testJSON.json'
         self.width = width
         self.height = height
-        self.fill(255,255,255)
+        self.fill((255,255,255))
         self.todaysClasses = []
         self.firstTimeSlot = ''
         self.firstTimeSlotClasses = []
@@ -16,20 +22,32 @@ class ScheduleDisplay(Surface):
         self.secondTimeSlotClasses = []
 
     def Update(self):
-    	pass
+        pass
 
-    def LoadTodaysClasses():
-    	pass
+    def LoadTodaysClasses(self):
+        self.todaysClasses = []
+        today = datetime.datetime.today().weekday()
+        daysOfWeek = ['M', 'T', 'W', 'Th', 'F', 'Sat', 'S']
+        with open(os.path.join(self.dir_path, self.scheduleFile)) as file:
+            data = json.loads(file.read())
+        for meeting in data:
+                if DoesClassMeet('M', meeting, 'LEC'): # <<<< Artificially made to monday, replace with daysOfWeek[today]
+                    self.todaysClasses.append(meeting)
+        self.todaysClasses = sorted(self.todaysClasses, key=lambda k: k['LEC']['Start']) 
+        for day in self.todaysClasses:
+            PrintClass(day)
 
-    def UpdateTimeSlotHeaders():
-    	pass
 
-    def UpdateClassList():
-    	pass
+    def UpdateTimeSlotHeaders(self):
+        pass
 
-    def CreateClassSurface():
-    	pass
+    def UpdateClassList(self):
+        pass
 
-    def GetNextTimeSlot():
+    def CreateClassSurface(self):
+        pass
+
+    def GetNextTimeSlot(self):
+        pass
 
 
---
+++ bcCodeRunners/scheduleDisplay/testScheduleDisplay.py
@@ -0,0 +1,33 @@
+# ---- TESTING -----
+import pygame
+from ScheduleDisplay import ScheduleDisplay
+from BCScheduleCreator import PrintClass
+
+
+
+def main():
+    pygame.init()
+    clock = pygame.time.Clock()
+    testScreen = pygame.display.set_mode((300, 300))
+    testScreen.fill((0,0,0))
+    schedule = ScheduleDisplay(100,100)
+    testScreen.blit(schedule, (0, 0))
+    schedule.LoadTodaysClasses()
+    pygame.display.update()
+    run = True
+    while(run):
+        clock.tick(26)
+        #testScreen.blit(ScheduleDisplay, (0, 0))
+        #pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == ord('q'):
+                    run = False
+            elif event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
