95a876d966c44cd979a842e916a15e2356c1ddbf
jdogmcsteezy <jesse.sasquatch.schmidt@gmail.com>
2018-01-20 14:12:35 -0800

    Create skeleton for surface object

--- bcCodeRunners/scheduleDisplay/ScheduleDisplay.py
+++ bcCodeRunners/scheduleDisplay/ScheduleDisplay.py
@@ -1,3 +1,35 @@
-from pygame import Surface
+from pygame import Surface, time, font
+import os
 
-class ScheduleDisplay
+class ScheduleDisplay(Surface):
+	def __init__(self, width, height):
+        Surface.__init__(self, (width, height))
+        font.init()
+        self.dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.width = width
+        self.height = height
+        self.fill(255,255,255)
+        self.todaysClasses = []
+        self.firstTimeSlot = ''
+        self.firstTimeSlotClasses = []
+        self.secondTimeSlot = ''
+        self.secondTimeSlotClasses = []
+
+    def Update(self):
+    	pass
+
+    def LoadTodaysClasses():
+    	pass
+
+    def UpdateTimeSlotHeaders():
+    	pass
+
+    def UpdateClassList():
+    	pass
+
+    def CreateClassSurface():
+    	pass
+
+    def GetNextTimeSlot():
+
+
